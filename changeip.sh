64fa99e4463a2709cd26e9194098dbbd26b04759
root <root@debian8.iutbeziers.fr>
2015-12-16 00:11:10 +0100

    script de creation du container

--- changeip.sh
+++ changeip.sh
@@ -10,3 +10,5 @@ mkdir -p /etc/docker/certs.d/$IPADDRS:$PORT
 mkdir -p /home/$MUSER/certs$IPADDRS
 openssl req -newkey rsa:4096 -nodes -sha256 -keyout /home/$MUSER/certs$IPADDRS/domain.key -x509 -days 1800 -out /home/$MUSER/certs$IPADDRS/domain.crt
 cp /home/$MUSER/certs$IPADDRS/domain.crt /etc/docker/certs.d/$IPADDRS:$PORT/ca.crt
+chown -R $MUSER.$MUSER /home/$MUSER/
+docker run -d -p $PORT:5000 --restart=always --name registry_$IPADDRS -v  /home/$MUSER/data:/var/lib/registry -v /home/$MUSER/certs$IPADDRS:/certs -e REGISTRY_HTTP_TLS_CERTIFICATE=/home/$MUSER/certs$IPADDRS/domain.crt -e REGISTRY_HTTP_TLS_KEY=/home/$MUSER/certs$IPADDRS/domain.key registry:2
