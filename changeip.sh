7f02fe3796b7214db6536a9fe67f8b41ee3823b0
root <root@debian8.iutbeziers.fr>
2015-12-16 00:56:17 +0100

    IFS...

--- changeip.sh
+++ changeip.sh
@@ -3,8 +3,13 @@ MUSER='pouchou'
 METH='eth1'
 PORT='5000'
 DEFAULT_DOCKER='/etc/default/docker'
+MYLINE='DOCKER_OPTS="—insecure-registry localhost:5000  —insecure-registry'
+LIGNE="$MYLINE $IPADDRS:$PORT \""
+echo $LIGNE
 IPADDRM=$(ip a|grep inet|grep $METH|awk '{print $2}')
+OLDIFS=IFS
 IFS=/; read -a IPADDRS <<<"$IPADDRM"
+IFS=OLDIFS
 sed -i "s/MONIP/$IPADDR/" ./openssl.cnf 
 cp ./openssl.cnf /etc/ssl/openssl.cnf
 mkdir -p /etc/docker/certs.d/$IPADDRS:$PORT
@@ -14,8 +19,6 @@ cp /home/$MUSER/certs$IPADDRS/domain.crt /etc/docker/certs.d/$IPADDRS:$PORT/ca.c
 chown -R $MUSER.$MUSER /home/$MUSER/
 DOCKER_OPTS="—insecure-registry localhost:5000  —insecure-registry $IPADDRS:$PORT"
 sed -i '/DOCKER_OPTS/d' $DEFAULT_DOCKER
-MYLINE='DOCKER_OPTS="—insecure-registry localhost:5000  —insecure-registry'
-LIGNE="$MYLINE $IPADDRS:$PORT \""
 echo $LIGNE >> $DEFAULT_DOCKER
 service docker restart
 docker run -d -p $PORT:5000 --restart=always --name registry_$IPADDRS -v  /home/$MUSER/data:/var/lib/registry -v /home/$MUSER/certs$IPADDRS:/certs -e REGISTRY_HTTP_TLS_CERTIFICATE=/home/$MUSER/certs$IPADDRS/domain.crt -e REGISTRY_HTTP_TLS_KEY=/home/$MUSER/certs$IPADDRS/domain.key registry:2
