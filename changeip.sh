79a4383bbcadc19da4187768fff0433880e302aa
root <root@debian8.iutbeziers.fr>
2015-12-16 23:59:21 +0100

    MONIP

--- changeip.sh
+++ changeip.sh
@@ -12,15 +12,15 @@ cp -f ./openssl.cnf /etc/ssl/openssl.cnf
 mkdir -p /etc/docker/certs.d/$IPADDRS:$PORT
 mkdir -p /home/$MUSER/certs$IPADDRS
 openssl req -newkey rsa:4096 -nodes -sha256 -keyout /home/$MUSER/certs$IPADDRS/domain.key -x509 -days 1800 -out /home/$MUSER/certs$IPADDRS/domain.crt
-cp /home/$MUSER/certs$IPADDRS/domain.crt /etc/docker/certs.d/$IPADDRS:$PORT/ca.crt
+cp "/home/$MUSER/certs$IPADDRS/domain.crt" "/etc/docker/certs.d/$IPADDRS:$PORT/ca.crt"
 chown -R $MUSER.$MUSER /home/$MUSER/
 REGSTRLINE="DOCKER_OPTS="
 REGSTRLINE+='"'
-REGSTRLINE+="—insecure-registry localhost:5000  —insecure-registry $IPADDRS:$PORT"
+REGSTRLINE+="--insecure-registry localhost:5000  --insecure-registry $IPADDRS:$PORT"
 REGSTRLINE+='"'
 echo $REGSTRLINE
 sed -i '/DOCKER_OPTS/d' $DEFAULT_DOCKER
 echo $REGSTRLINE >> $DEFAULT_DOCKER
 service docker restart
+echo "run -d -p $PORT:5000 --restart=always --name registry_$IPADDRS -v  /home/$MUSER/data:/var/lib/registry -v /home/$MUSER/certs$IPADDRS:/certs -e REGISTRY_HTTP_TLS_CERTIFICATE=/home/$MUSER/certs$IPADDRS/domain.crt -e REGISTRY_HTTP_TLS_KEY=/home/$MUSER/certs$IPADDRS/domain.key registry:2"
 docker run -d -p $PORT:5000 --restart=always --name registry_$IPADDRS -v  /home/$MUSER/data:/var/lib/registry -v /home/$MUSER/certs$IPADDRS:/certs -e REGISTRY_HTTP_TLS_CERTIFICATE=/home/$MUSER/certs$IPADDRS/domain.crt -e REGISTRY_HTTP_TLS_KEY=/home/$MUSER/certs$IPADDRS/domain.key registry:2
-
